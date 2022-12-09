2db56eeb0acaf54c6bc83c76133862e0fb1e1edf
Budaházi Bence <budahazi.bence@petrik.hu>
2022-12-09 16:41:10 +0100

    Majom hozzáadása

--- src/main/java/hu/petrik/adatbazisjavafx/MajmokController.java
+++ src/main/java/hu/petrik/adatbazisjavafx/MajmokController.java
@@ -33,7 +33,7 @@ public class MajmokController {
         fajtaOszlop.setCellValueFactory(new PropertyValueFactory<>("fajta"));
         max_iqOszlop.setCellValueFactory(new PropertyValueFactory<>("max_iq"));
         szereti_banantOszlop.setCellValueFactory(new PropertyValueFactory<>("szereti_banantDisplay"));
-        max_iqInput.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0,255, 50));
+        max_iqInput.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 255, 50));
         Platform.runLater(() -> {
             try {
                 db = new MajomDB();
@@ -60,5 +60,31 @@ public class MajmokController {
 
     @FXML
     public void elkuldClick(ActionEvent actionEvent) {
+        String fajta = fajtaInput.getText().trim();
+        int max_iq = max_iqInput.getValue();
+        boolean szereti_banant = szereti_banantCheckbox.isSelected();
+        if (fajta.isEmpty()) {
+            alert(Alert.AlertType.WARNING, "Fajta megadása kötelező", "");
+            return;
+        }
+        Majom majom = new Majom(fajta, max_iq, szereti_banant);
+        try {
+            db.majomHozzaadasa(majom);
+            alert(Alert.AlertType.WARNING, "Sikeres felvétel", "");
+            majmokListazasa();
+            urlapAlaphelyzetbe();
+        } catch (SQLException e) {
+            if (e.getErrorCode() == 1062) {
+                alert(Alert.AlertType.WARNING, "Ilyen fajtájú majom már van felvéve", "");
+            } else {
+                alert(Alert.AlertType.ERROR, "Adatbazis hiba", e.getMessage());
+            }
+        }
+    }
+
+    private void urlapAlaphelyzetbe() {
+        fajtaInput.setText("");
+        max_iqInput.getValueFactory().setValue(50);
+        szereti_banantCheckbox.setSelected(false);
     }
 }
